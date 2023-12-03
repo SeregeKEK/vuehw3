8ce0ce678b082cc3c4d61411a111d71fc37b4ccc
SeregeKEK <kurnakov2012@mail.ru>
2023-12-04 00:08:59 +0300

    vuehw

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <div v-for="product in products" :key="product.id">
+            <h2> {{ product.name }}</h2>
+            <span> цена: {{ formattedPrice(product.price) }}</span>
+
+            <div v-if="product.available">
+                <span> Availeble</span>
+            </div>
+            <div v-else>
+                <span> Out of stock</span>
+            </div>
+            <br>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductDetails',
+
+  data () {
+    return {
+      moneta: '',
+      products: [
+        {
+          id: 1,
+          name: 'duster',
+          price: '13.99',
+          available: true
+        },
+        {
+          id: 2,
+          name: 'tv',
+          price: '99.99',
+          available: true
+        },
+        {
+          id: 3,
+          name: 'phone',
+          price: '50',
+          available: false
+        },
+        {
+          id: 4,
+          name: 'camera',
+          price: '25.50',
+          available: true
+        }
+      ]
+    }
+  },
+  computed: {
+    formattedPrice () {
+      return price => `$ ${price}`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
